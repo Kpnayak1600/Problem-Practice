18f4bd30548e80cbf94b855e37adb84bd94b4cbb
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-01-21 01:06:49 +0530

    Added solution - LeetHub

--- Sorted matrix - GFG/sorted-matrix.java
+++ Sorted matrix - GFG/sorted-matrix.java
@@ -33,18 +33,27 @@ class GFG {
 class Solution {
     int[][] sortedMatrix(int N, int Mat[][]) {
         // code here
-         PriorityQueue<Integer> q = new PriorityQueue<Integer>();
-         for(int[] i:Mat){
-             for(int j=0;j<i.length;j++){
-                 q.add(i[j]);
-             }
-         }
-         int res[][] = new int[N][N];
-         for(int i=0;i<res.length;i++){
-             for(int j=0;j<res[0].length;j++){
-                 res[i][j] = q.poll();
-             }
-         }
-         return res;
+        List<Integer> set = new ArrayList<>(N*N);
+		
+        
+        for(int row=0;row<N;row++) {
+            for(int col=0;col<N;col++) {
+                set.add(Mat[row][col]);
+            }
+        }
+        
+        int[] re = set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        int idx = 0;
+        int[][] result = new int[N][N];
+        
+          for(int row=0;row<N;row++) {
+            for(int col=0;col<N;col++) {
+                result[row][col] = re[idx];
+                idx++;
+            }
+        }
+        
+        return result;
+    
     }
 };

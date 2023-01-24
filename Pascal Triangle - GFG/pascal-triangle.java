2aa8cef560992aec84d7f935972aaa774948f621
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-01-24 17:17:56 +0530

    Added solution - LeetHub

--- Pascal Triangle - GFG/pascal-triangle.java
+++ Pascal Triangle - GFG/pascal-triangle.java
@@ -33,20 +33,34 @@ class GFG {
 class Solution {
     ArrayList<Long> nthRowOfPascalTriangle(int n) {
         // code here
+        // List<ArrayList<Long>>a =new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     ArrayList<Long> r = new ArrayList<>();
+        //     for(int j=0;j<=i;j++){
+        //         if(i<2 || j==i || j==0){
+        //             r.add((long)1);
+        //             continue;
+        //         }
+        //         r.add((a.get(i-1).get(j-1)+a.get(i-1).get(j))%1000000007);
+        //     }
+        //     a.add(new ArrayList<Long>(r));
+            
         List<ArrayList<Long>>a =new ArrayList<>();
+        ArrayList<Long> prev = null;
         for(int i=0;i<n;i++){
-            ArrayList<Long> r = new ArrayList<>();
+            ArrayList<Long> curr = new ArrayList<>();
             for(int j=0;j<=i;j++){
                 if(i<2 || j==i || j==0){
-                    r.add((long)1);
+                    curr.add((long)1);
                     continue;
                 }
-                r.add((a.get(i-1).get(j-1)+a.get(i-1).get(j))%1000000007);
+                curr.add((prev.get(j-1)+prev.get(j))%1000000007);
             }
-            a.add(new ArrayList<Long>(r));
+            a.add(new ArrayList<Long>(curr));
+            prev = curr;
+            //System.out.println(prev);
         }
         //System.out.println(a);
-        //ArrayList<Long> d = new ArrayList<>();
         return a.get(n-1);
     }
 }

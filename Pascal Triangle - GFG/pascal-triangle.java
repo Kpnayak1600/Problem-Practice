abfd189c6d47f49857f6f11c70664516003d4af5
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-01-24 18:31:45 +0530

    Added solution - LeetHub

--- Pascal Triangle - GFG/pascal-triangle.java
+++ Pascal Triangle - GFG/pascal-triangle.java
@@ -33,22 +33,9 @@ class GFG {
 class Solution {
     ArrayList<Long> nthRowOfPascalTriangle(int n) {
         // code here
-        // List<ArrayList<Long>>a =new ArrayList<>();
-        // for(int i=0;i<n;i++){
-        //     ArrayList<Long> r = new ArrayList<>();
-        //     for(int j=0;j<=i;j++){
-        //         if(i<2 || j==i || j==0){
-        //             r.add((long)1);
-        //             continue;
-        //         }
-        //         r.add((a.get(i-1).get(j-1)+a.get(i-1).get(j))%1000000007);
-        //     }
-        //     a.add(new ArrayList<Long>(r));
-            
-        List<ArrayList<Long>>a =new ArrayList<>();
-        ArrayList<Long> prev = null;
+        ArrayList<Long> prev = null,curr = null;
         for(int i=0;i<n;i++){
-            ArrayList<Long> curr = new ArrayList<>();
+            curr = new ArrayList<>();
             for(int j=0;j<=i;j++){
                 if(i<2 || j==i || j==0){
                     curr.add((long)1);
@@ -56,11 +43,10 @@ class Solution {
                 }
                 curr.add((prev.get(j-1)+prev.get(j))%1000000007);
             }
-            a.add(new ArrayList<Long>(curr));
             prev = curr;
-            //System.out.println(prev);
+           // System.out.println(prev);
         }
         //System.out.println(a);
-        return a.get(n-1);
+        return prev;
     }
 }

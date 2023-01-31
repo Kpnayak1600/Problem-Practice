27cc1848b620e62080c1b4342bc54b800f375140
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-01-31 14:24:00 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            obj.Rearrange(a, n);
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<n;i++)
+                output.append(a[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    public void Rearrange(long arr[], long n){
+        // Your code goes here
+        long temp[] = arr.clone();
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(temp[i]<0)   {
+                arr[k++] =temp[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(temp[i]>=0)   {
+                arr[k++] =temp[i];
+            }
+        }
+    }
+}

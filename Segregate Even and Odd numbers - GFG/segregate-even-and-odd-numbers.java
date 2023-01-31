b7c8178b793fa0535c8d84fac888a9e9ae49d04c
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-01-31 13:05:31 +0530

    Added solution - LeetHub

---
+++ Segregate Even and Odd numbers - GFG/segregate-even-and-odd-numbers.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().segregateEvenOdd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void segregateEvenOdd(int arr[], int n) {
+        // code here
+        int i=0;
+        for(int j=0;j<n;j++){
+              if(arr[j]%2==0){
+                  int temp = arr[j];
+                  arr[j] = arr[i];
+                  arr[i] = temp;
+                  i++;
+              }
+        }
+        Arrays.sort(arr,0,i);
+        Arrays.sort(arr,i,arr.length);
+    }
+}

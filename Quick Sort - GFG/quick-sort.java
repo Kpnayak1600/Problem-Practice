411d93e2b69fd7f70b6cb0aaa1b2a89dd5677a1a
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-02-01 20:40:39 +0530

    Added solution - LeetHub

--- Quick Sort - GFG/quick-sort.java
+++ Quick Sort - GFG/quick-sort.java
@@ -31,26 +31,25 @@ class Sorting
 // } Driver Code Ends
 
 
+
+
 class Solution{
     //Function to sort an array using quick sort algorithm.
     static int pivot(int[]arr,int l,int r){
-        int i=l-1;
-        int j=r+1;
-        int pivot= arr[l];
-         while (true) {
-            do{
+        int pivot = arr[r];
+        int i=l;
+        for(int j=l;j<=r;j++){
+            if(arr[j]<pivot){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
                 i++;
-            }while(arr[i]<pivot);
-            do{
-                j--;
-            }while(arr[j]>pivot);
-            if(i>=j){
-                return j;
             }
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
         }
+        int temp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = temp;
+        return i;
     }
     static void quickSort(int arr[], int low, int high){
         // code here
@@ -58,7 +57,7 @@ class Solution{
             return ;
         }
         int pivot = pivot(arr,low,high);
-        quickSort(arr, low, pivot);
+        quickSort(arr, low, pivot-1);
         quickSort(arr, pivot+1, high);
     }
 }

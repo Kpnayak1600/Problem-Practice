41860bcbe69081eb1e81b3a7b78ebeaf5bbc143f
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-02-19 15:37:31 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.java
+++ Rotation - GFG/rotation.java
@@ -42,7 +42,9 @@ class Solution {
                 return mid;
             }else if(arr[mid]<=arr[end]){
                 end = mid-1;
-            }else if(arr[mid]>=arr[start]){
+            }
+            //else if(arr[mid]>=arr[start]){
+            else {
                 start = mid+1;
             }
         }

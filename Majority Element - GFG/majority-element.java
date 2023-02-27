e50d4ea6619b8b14e5c9e6dc1856fa6515fe3b32
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-02-27 20:47:42 +0530

    Added solution - LeetHub

---
+++ Majority Element - GFG/majority-element.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int majorityElement(int a[], int size){
+        // your code here
+        int n = a.length;
+        int res = a[0];
+        int count = 1;
+        for(int i=1;i<n;i++){
+            if(a[i]==res){
+                count++;
+            }else if(a[i]!=res){
+                count--;
+            }
+            if(count == 0){
+                count = 1;
+                res = a[i];
+            }
+        }
+        count = 0;
+        for(int i=0;i<n;i++){
+            if(res == a[i]){
+                count++;
+            }
+        }
+        return count>(n/2)?res:-1;
+    }
+}

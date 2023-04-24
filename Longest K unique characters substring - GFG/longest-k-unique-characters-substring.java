fd2e72a4d25f48d832e527f74da5c0081c82cf6a
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-04-24 21:29:25 +0530

    Added solution - LeetHub

---
+++ Longest K unique characters substring - GFG/longest-k-unique-characters-substring.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            int k = sc.nextInt();
+            Solution obj = new Solution();
+            System.out.println(obj.longestkSubstr(s, k));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int longestkSubstr(String s, int k) {
+        // code here
+        int i=0,j=0;
+        int res = -1;
+        Map<Character,Integer> mp = new HashMap<>();
+        while(j<s.length()){
+            char c = s.charAt(j);
+            mp.put(c,mp.getOrDefault(c,0)+1);
+            if(mp.size()<k){
+                j++;
+            }else if(mp.size() == k){
+                res = Math.max(res,j-i+1);
+                j++;
+            }else if(mp.size()>k){
+                if(mp.containsKey(s.charAt(i))){
+                    mp.put(s.charAt(i),mp.get(s.charAt(i))-1);
+                }
+                if(mp.get(s.charAt(i))==0){
+                    mp.remove(s.charAt(i));
+                }
+                j++;i++;
+            }
+        }
+        return res;
+    }
+}

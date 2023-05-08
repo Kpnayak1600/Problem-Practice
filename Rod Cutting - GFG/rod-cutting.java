2e2b04a16be6d3ac11e906f4d00c819ed65e3672
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-05-09 01:29:52 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -23,26 +23,27 @@ class RodCutting {
 
 class Solution{
     public int cutRod(int price[], int n) {
-        int dp[][] = new int[price.length+1][n+1];
-        for(int i[]: dp){
-            Arrays.fill(i,-1);
-        }
-        return helper(price,price.length,n,dp);
+        return helper(price,price.length,n);
     }
-    int helper(int[]price,int ind,int len,int[][]dp){
-        if(ind==0){
-            return 0;
+    int helper(int[]price,int ind,int len){
+        int[][]dp = new int[ind+1][len+1];
+        for(int i=0;i<ind+1;i++){
+            for(int j=0;j<len+1;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                }
+                if(i>0&&j>0){
+                    // take 
+                    int take = Integer.MIN_VALUE;
+                    if(j>=i){
+                        take = price[i-1]+dp[i][j-i];
+                    }
+                    // not take
+                    int ntake = dp[i-1][j];
+                    dp[i][j] = Math.max(take,ntake);
+                }
+            }
         }
-        if(dp[ind][len]!=-1){
-            return dp[ind][len];
-        }
-        // take 
-        int take = Integer.MIN_VALUE;
-        if(len>=ind){
-            take = price[ind-1]+helper(price,ind,len-ind,dp);
-        }
-        // not take
-        int ntake = helper(price,ind-1,len,dp);
-        return dp[ind][len]=Math.max(take,ntake);
+        return dp[ind][len];
     }
 }

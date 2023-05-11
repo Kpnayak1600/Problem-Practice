77ef8fc625df413c13dd3d5540b991fd3b8cf282
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-05-11 11:43:33 +0530

    Added solution - LeetHub

--- Longest Common Substring - GFG/longest-common-substring.java
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -29,39 +29,37 @@ class GFG
 
 //User function Template for Java
 
+
+
+//User function Template for Java
+
 class Solution{
     int ans=0;
-    int solve(String s1,String s2,int n,int m,int[][] dp)
-    {
-        if(n==s1.length()||m==s2.length())
-        {
+    int solve(String s1,String s2,int n,int m,int[][] dp){
+        if(n==0||m==0){
             return 0;
         }
-        if(dp[n][m]!=-1)
-        {
+        if(dp[n][m]!=-1){
             return dp[n][m];
         }
-        // int b=0;
-        // int c=0;
         int count=0;
-        if(s1.charAt(n)==s2.charAt(m))
-        {
-            count=1+solve(s1,s2,n+1,m+1,dp);
+        if(s1.charAt(n-1)==s2.charAt(m-1)){
+            count=1+solve(s1,s2,n-1,m-1,dp);
             ans=Math.max(ans,count);
         }
-        solve(s1,s2,n+1,m,dp);
-        solve(s1,s2,n,m+1,dp);
+        solve(s1,s2,n-1,m,dp);
+        solve(s1,s2,n,m-1,dp);
         return dp[n][m]=count;
        
     }
     int longestCommonSubstr(String s1, String s2, int n, int m){
         // code here
-        int dp[][]=new int[n][m];
+        int dp[][]=new int[n+1][m+1];
         for(int[] arr:dp)
         {
             Arrays.fill(arr,-1);
         }
-        solve(s1,s2,0,0,dp);
+        solve(s1,s2,n,m,dp);
         return ans;
     }
 }

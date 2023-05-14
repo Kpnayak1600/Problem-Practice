e3e941851b75efc6acb27ed0244c183204ae67e6
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-05-14 10:25:31 +0530

    Added solution - LeetHub

---
+++ Boolean Parenthesization - GFG/boolean-parenthesization.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String S = in.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countWays(N, S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int countWays(int N, String S){
+        // code here
+        int dp[][][] = new int[N+1][N+1][2];
+        for(int i[][]:dp){
+            for(int j[]:i){
+                Arrays.fill(j,-1);
+            }
+        }
+        return helper(S,0,N-1,1,dp);
+    }
+    static int helper(String S,int i,int j,int isTrue,int dp[][][]){
+        if(i>j){
+            return 0;
+        }
+        if(i==j){
+            if(isTrue==1){
+                return S.charAt(i)=='T'?1:0;
+            }else{
+                return S.charAt(i)=='F'?1:0;
+            }
+        }
+        if(dp[i][j][isTrue]!=-1){
+            return dp[i][j][isTrue];
+        }
+        int ans = 0;
+        for(int k=i+1;k<j;k+=2){
+            int leftT = helper(S,i,k-1,1,dp);
+            int leftF = helper(S,i,k-1,0,dp);
+            int rightT = helper(S,k+1,j,1,dp);
+            int rightF = helper(S,k+1,j,0,dp);
+            if(S.charAt(k)=='&'){
+                ans += isTrue == 1 ? leftT*rightT : leftF*rightF + leftT*rightF + leftF*rightT;
+            }else if(S.charAt(k)=='|'){
+                ans += isTrue == 1 ? leftT*rightT + leftT*rightF + leftF*rightT : leftF*rightF;
+            }else if(S.charAt(k)=='^'){
+                ans += isTrue == 1 ? leftT*rightF + leftF*rightT : leftT*rightT + leftF*rightF;
+            }
+        }
+        return dp[i][j][isTrue]=ans%1003;
+    }
+}

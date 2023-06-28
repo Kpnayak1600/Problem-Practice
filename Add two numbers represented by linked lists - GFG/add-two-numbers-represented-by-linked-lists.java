9d827b7c24c8d703aac6a335666c43fdec3c576e
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-06-28 18:15:30 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
@@ -79,21 +79,21 @@ class Node {
 
 class Solution{
     //Function to add two numbers represented by linked list.
-    static Node addTwoLists(Node l1, Node l2){
+    static Node addTwoLists(Node list1, Node list2){
         // code here
         // return head of sum list
-        l1 = reverse(l1);
-        l2 = reverse(l2);
+        list1 = reverse(list1);
+        list2 = reverse(list2);
         Node dummy = new Node (-1);
         Node temp = dummy;
         int carry = 0;
-        while(l1!=null||l2!=null||carry!=0){
+        while(list1!=null||list2!=null||carry!=0){
             int sum =0;
-            if(l1!=null){
-                sum += l1.data; l1 = l1.next;
+            if(list1!=null){
+                sum += list1.data; list1 = list1.next;
             }
-            if(l2!=null){
-                sum+=l2.data; l2 = l2.next;
+            if(list2!=null){
+                sum+=list2.data; list2 = list2.next;
             }
             sum+=carry;
             carry = sum/10;

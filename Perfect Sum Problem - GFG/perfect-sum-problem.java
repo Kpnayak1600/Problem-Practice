ccf2f4ba242aa690c6d95b42d3c1fae3659fa4b8
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-06-29 12:37:04 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -28,24 +28,22 @@ class GfG
 class Solution{
 	public int perfectSum(int arr[],int n, int sum) { 
 	    // Your code goes here
-	   int dp[][] = new int[n+1][sum+1];
-	   for(int i=0;i<n+1;i++){
-	       for(int j=0;j<sum+1;j++){
-	           if(i==0&&j==0){
-	               dp[i][j] = 1;
-	           }
-	           if(i>0){
-	               // take
-	               int take = 0;
-	               if(j>=arr[i-1]){
-	                   take = dp[i-1][j-arr[i-1]];
-	               }
-	               // not take
-	               int ntake = dp[i-1][j];
-	               dp[i][j] = (take+ntake)%1000000007;
-	           }
+	   int prev [] = new int [sum+1];
+	   prev[0] = 1;
+	   for(int i=1;i<n+1;i++){
+	       int cur[] = new int[sum+1];
+	       for(int j=0;j<sum+1;j++){ 
+               // take
+               int take = 0;
+               if(j>=arr[i-1]){
+                   take = prev[j-arr[i-1]];
+               }
+               // not take
+               int ntake = prev[j];
+               cur[j] = (take+ntake)%1000000007;
 	       }
+	       prev = cur;
 	   }
-	   return dp[n][sum];
+	   return prev[sum];
 	} 
 }

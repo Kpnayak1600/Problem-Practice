17a6746c1fb006e1e16d77c842f1b7e988e508cf
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-07-01 13:55:58 +0530

    Added solution - LeetHub

---
+++ N-Queen Problem - GFG/nqueen-problem.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
+            if(ans.size() == 0)
+                System.out.println("-1");
+            else {
+                for(int i = 0;i < ans.size();i++){
+                    System.out.print("[");
+                    for(int j = 0;j < ans.get(i).size();j++)
+                        System.out.print(ans.get(i).get(j) + " ");
+                    System.out.print("] ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        // code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        int[][] grid = new int[n][n];
+        nQueenHelper(res,grid,0);
+        return res;
+    }
+    static void nQueenHelper(ArrayList<ArrayList<Integer>>res,int[][] grid,int row){
+        if(row==grid[0].length){
+            res.add(possibleSolution(grid));
+            return ;
+        }
+        for(int col=0;col<grid.length;col++){
+            if(isValid(row,col,grid)){
+                grid[row][col] = 1;
+                nQueenHelper(res,grid,row+1);
+                grid[row][col] = 0;
+            }
+        }
+    }
+    static boolean isValid(int row,int col,int grid[][]){
+        int i=row;
+        int j=col;
+        while(i>=0&&j>=0){
+            if(grid[i--][j--]==1){
+                return false;
+            }
+        }
+        i=row;
+        while(i>=0){
+            if(grid[i--][col]==1){
+                return false;
+            }
+        }
+        i=row;
+        j=col;
+        while(i>=0 && j<grid.length){
+            if(grid[i--][j++]==1){
+                return false;
+            }
+        }
+         return true;
+    }
+    static ArrayList<Integer> possibleSolution(int[][]grid){
+        ArrayList<Integer> rs = new ArrayList<>();
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    rs.add(j+1);
+                }
+            }
+        }
+        return rs;
+    }
+}

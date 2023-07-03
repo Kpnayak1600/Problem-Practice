e1a589618fe4e618afea2e68087bbec8f2af6e3b
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-07-03 12:11:09 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.java
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.java
@@ -124,26 +124,27 @@ class Tree{
     //Function to return list containing elements of left view of binary tree.
     ArrayList<Integer> leftView(Node root){
       // Your code here
-      ArrayList<Integer> res = new ArrayList<>();
-      int maxLevel =0;
-      int currentLevel=1;
-      helper(res,root,currentLevel);
-      return res;
-    }
-    int maxLevel=0;
-    void helper(ArrayList<Integer> res,Node root,int currentLevel){
+        ArrayList<Integer> res = new ArrayList<>();
         if(root==null){
-            return;
+            return res;
         }
-        // go left
-        if(maxLevel<currentLevel){
-            res.add(root.data);
-            maxLevel=currentLevel;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                Node cur = q.poll();
+                if(i==0){
+                    res.add(cur.data);
+                }
+                if(cur.left!=null){
+                    q.add(cur.left);
+                }
+                if(cur.right!=null){
+                    q.add(cur.right);
+                }
+            }
         }
-        helper(res,root.left,currentLevel+1);
-        //System.out.println(maxLevel);
-        // go right
-        helper(res,root.right,currentLevel+1);
-        //System.out.println(maxLevel);
+        return res;
     }
 }

c0cde9216ca99f53440fd6f58ef8f1369f822fa1
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-07-05 14:20:49 +0530

    Added solution - LeetHub

---
+++ Vertical sum - GFG/vertical-sum.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    ArrayList <Integer> res = g.verticalSum(root);
+			    for (Integer num : res) System.out.print (num + " ");
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+int data;
+Node left, right;
+Node(int item)
+{
+    data = item;
+    left = right = null
+}
+}
+*/
+class pair{
+    int d;
+    Node node;
+    pair(int d,Node node){
+        this.d = d;
+        this.node = node;
+    }
+}
+class Solution{
+    public ArrayList <Integer> verticalSum(Node root) {
+        Map<Integer,ArrayList<Integer>> mp = new TreeMap<>();
+        Queue<pair> q = new LinkedList<>();
+        q.add(new pair(0,root));
+        while(!q.isEmpty()){
+            pair cur = q.poll();
+            if(mp.containsKey(cur.d)){
+                mp.get(cur.d).add(cur.node.data);
+            }else{
+                ArrayList<Integer> a = new ArrayList<>();
+                a.add(cur.node.data);
+                mp.put(cur.d,a);
+            }
+            if(cur.node.left!=null){
+                q.add(new pair(cur.d-1,cur.node.left));
+            }
+            if(cur.node.right!=null){
+                q.add(new pair(cur.d+1,cur.node.right));
+            }
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        //System.out.println(mp);
+        int sum =0;
+        for(ArrayList<Integer>i : mp.values()){
+            for(Integer x : i){
+                sum+=x;
+            }
+            res.add(sum);
+            sum=0;
+        }
+        return res;
+    }
+}
+
+

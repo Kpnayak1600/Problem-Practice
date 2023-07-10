60e340fa38fa222eee91e476be5c1a4b4e9ddff4
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-07-11 01:14:15 +0530

    Added solution - LeetHub

--- Maximum Path Sum between 2 Special Nodes - GFG/maximum-path-sum-between-2-special-nodes.java
+++ Maximum Path Sum between 2 Special Nodes - GFG/maximum-path-sum-between-2-special-nodes.java
@@ -137,10 +137,7 @@ class Solution
     
     int maxPathSumUtil(Node root) {
         if (root == null) 
-            return 0;
-        // if (root.left == null && root.right == null) 
-        //     return root.data;
-
+            return 0
         int ls = maxPathSumUtil(root.left);
         int rs = maxPathSumUtil(root.right);
 

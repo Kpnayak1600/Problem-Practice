8be318ba7df699e240f042a19d9d9b72d7bd0552
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-08-11 18:14:44 +0530

    Added solution - LeetHub

--- Find the element that appears once - GFG/find-the-element-that-appears-once.java
+++ Find the element that appears once - GFG/find-the-element-that-appears-once.java
@@ -27,15 +27,31 @@ class GfG
 // } Driver Code Ends
 
 
-//User function Template for Java
+//Back-end complete function Template for Java
 
 class Sol{
-    public static int search(int A[], int N){
-        // your code here
-        int res = A[0];
-        for(int i=1;i<N;i++){
-            res^=A[i];
+    public static int bs(int arr[], int low, int high){
+        if (low==high){
+            return arr[low];
         }
-        return res;
+        int mid = (low + high) / 2;
+        // If mid is even and element next to mid is
+        // same as mid, then output element lies on
+        // right side, else on left side
+        if (mid%2 == 0){
+            if (arr[mid] == arr[mid+1])
+                return bs(arr, mid+2, high);
+            else
+                return bs(arr, low, mid);
+        }
+        else{  // If mid is odd
+            if (arr[mid] == arr[mid-1])
+                return bs(arr, mid+1, high);
+            else
+                return bs(arr, low, mid-1);
+        }
+    }
+    public static int search(int arr[], int n){
+        return bs(arr,0,n-1);
     }
 }

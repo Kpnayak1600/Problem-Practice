9083346516ca286fe1753cd67472268e91fb08f5
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-08-12 11:08:39 +0530

    Added solution - LeetHub

--- Rotation - GFG/rotation.java
+++ Rotation - GFG/rotation.java
@@ -29,21 +29,19 @@ class GFG {
 
 // User function Template for Java
 
-
 class Solution {
     int findKRotation(int arr[], int n) {
         // code here
-        int start =0;
-        int end = n-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            //System.out.println(start+" "+end+" "+mid+" "+arr[mid]);
-            if(arr[mid]<=arr[(mid+1)%n]&&arr[mid]<=arr[(mid+n-1)%n]){
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(arr[mid]<arr[(mid+1)%n] && arr[mid]<arr[(mid+n-1)%n]){
                 return mid;
             }else if(arr[mid]<arr[0]){
-                end = mid-1;
+                j=mid-1;
             }else{
-                start = mid+1;
+                i=mid+1;
             }
         }
         return 0;

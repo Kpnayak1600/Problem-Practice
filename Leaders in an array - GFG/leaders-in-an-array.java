6062e41f59dde7cb4e7bea976ae83d851b265f01
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-08-18 13:01:12 +0530

    Added solution - LeetHub

--- Leaders in an array - GFG/leaders-in-an-array.java
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -49,16 +49,14 @@ class Solution{
     //Function to find the leaders in the array.
     static ArrayList<Integer> leaders(int arr[], int n){
         // Your code here
-        ArrayList<Integer> a = new ArrayList<>();
-        int max = arr[n-1];
-        a.add(max);
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(arr[n-1]);
         for(int i=n-2;i>=0;i--){
-            if(arr[i]>=max){
-                max = arr[i];
-                a.add(max);
+            if(arr[i]>=res.get(res.size()-1)){
+                res.add(arr[i]);
             }
         }
-        Collections.reverse(a);
-        return a;
+        Collections.reverse(res);
+        return res;
     }
 }

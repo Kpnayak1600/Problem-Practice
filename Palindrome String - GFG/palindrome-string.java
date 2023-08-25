d01d3ebd349479d98dc3765c156547b2bac522ab
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-08-25 21:16:06 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.java
+++ Palindrome String - GFG/palindrome-string.java
@@ -25,13 +25,8 @@ class GFG {
 class Solution {
     int isPalindrome(String S) {
         // code here
-        int i=0;
-        int j=S.length()-1;
-        while(i<j){
-            if(S.charAt(i++)!=S.charAt(j--)){
-                return 0;
-            }
-        }
-        return 1;
+        StringBuffer str = new StringBuffer(S);
+        str = str.reverse();
+        return str.toString().equals(S) ? 1:0;
     }
 };

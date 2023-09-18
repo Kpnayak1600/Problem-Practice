885484f2779d9f293851e03212a23e0f91b4afae
Kpnayak1600 <84857735+Kpnayak1600@users.noreply.github.com>
2023-09-18 20:53:02 +0530

    Added solution - LeetHub

---
+++ Power of 2 - GFG/power-of-2.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// //Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driver_class
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());//testcases
+        
+        while(t-- > 0)
+        {
+            //input a number n
+            long n = Long.parseLong(read.readLine());
+            
+            
+            // if n is less than equal to zero then 
+            //it can't be a power of 2 so we print No
+            if(new Solution().isPowerofTwo(n) == true)
+              System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n){
+        // Your code here
+        long i = 0;
+        long j = n;
+        while(i<=j){
+            long mid = i-(i-j)/2;
+            long x = (long)Math.pow(2,mid);
+            //System.out.println(mid+" "+x);
+            if(x==n){
+                return true;
+            }
+            if(x<n){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+        return false;
+    }
+}
